5251430c84a0f2466ecc29b9633c8b9d68ba892c
Leonidas Amorim <leonidasamorim@gmail.com>
2020-02-18 22:40:52 -0300

    imagens

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
-
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
-
-export default class App extends React.Component {
+export default class JustifyContentBasics extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.paragraph}>
-          Alterado o texto de teste code in the editor ok watch it change on your phone! Save to get a shareable url.
-        </Text>
-        <Card>
-          <AssetExample />
-        </Card>
+        <View style={styles.boxTopo} />
+        <View style={styles.row}>
+          <View style={styles.column} />
+          <View style={styles.column} />
+          <View style={styles.column} />
+        </View>
+        <View style={styles.row}>
+          <View style={styles.column} />
+          <View style={styles.column} />
+          <View style={styles.column} />
+        </View>
+        <View style={styles.row}>
+          <View style={styles.column} />
+          <View style={styles.column} />
+          <View style={styles.column} />
+        </View>
+        <View style={styles.boxRodape} />
       </View>
     );
   }
@@ -26,15 +30,31 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#472e99',
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
+  row: {
+    backgroundColor: 'red',
+    flexDirection: 'row',
+  },
+  column: {
+    margin: 10,
+    width: 80,
+    height: 80,
+    backgroundColor: 'steelblue',
+  },
+  boxTopo: {
+    marginTop: 10,
+    width: 300,
+    height: 80,
+    backgroundColor: 'yellow',
+  },
+  boxRodape: {
+    marginTop: 0,
+    width: 300,
+    height: 80,
+    backgroundColor: 'white',
   },
 });
